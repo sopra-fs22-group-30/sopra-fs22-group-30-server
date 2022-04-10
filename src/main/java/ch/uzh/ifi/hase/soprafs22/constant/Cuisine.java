f8e5a2ec68805e91c3822d59a6cee29dc8c40b35
Duanran Jing <duanran.jing@uzh.ch>
2022-04-10 16:15:26 +0200

    :hammer: update cuisine

--- src/main/java/ch/uzh/ifi/hase/soprafs22/constant/Cuisine.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/constant/Cuisine.java
@@ -1,5 +1,5 @@
 package ch.uzh.ifi.hase.soprafs22.constant;
 
 public enum Cuisine {
-    Chinese, Italian, Dessert;
+    Mexican, Swedish, Latvian, Italian, Spanish, American, Scottish, British, Thai, Japanese, Chinese, Indian, Canadian, Russian, Jewish, Polish, German, French, Hawaiian, Brazilian, Peruvian, Salvadorian, Cuban, Tibetan, Egyptian, Greek, Belgian, Irish, Welsh, Mormon, Cajun, Portuguese, Turkish, Haitian, Tahitian, Kenyan, Korean, Algerian, Nigerian, Libyan, Others
 }
