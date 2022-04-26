68b921c6fcc5a8d915385615f607fd0c90302523
HJGuan <HJGuan@users.noreply.github.com>
2022-04-26 21:19:09 +0200

    websocket

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/repository/ChecklistGetDTORepository.java
@@ -0,0 +1,13 @@
+//package ch.uzh.ifi.hase.soprafs22.repository;
+//
+//import ch.uzh.ifi.hase.soprafs22.entity.Ingredient;
+//import ch.uzh.ifi.hase.soprafs22.websocket.ChecklistGetDTO;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.UUID;
+//
+//@Repository("checklistGetDTORepository")
+//public interface ChecklistGetDTORepository extends JpaRepository<ChecklistGetDTO, UUID> {
+//
+//}
